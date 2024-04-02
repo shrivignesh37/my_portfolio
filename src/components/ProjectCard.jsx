6d41007bd35c1baf98e665cf0368badd4d9807d0
Blacksquad007 <112714180+Blacksquad007@users.noreply.github.com>
2024-04-03 00:10:19 +0530

    project card 1.1

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,6 +5,14 @@ import './css/ProfileCard.css';
 import { MDBCard} from 'mdb-react-ui-kit';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
 
 export default function ProjectCard() {
   return (
@@ -38,7 +46,28 @@ export default function ProjectCard() {
             <div >
             <div >
             <div className="col-lg-0 offset-lg-0" style={{marginRight:'12vh', marginTop:'10vh', width:'110vh',height:'70.5vh'}}>
-           
+
+            <Card style={{ maxWidth: 345 , marginLeft:'-70vh' , marginTop:"-0.90vh" }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
             </div>
       </div>
     </div>
